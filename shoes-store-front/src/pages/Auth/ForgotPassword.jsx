787a37457a1c6e775eac89d7563bf4aa77f9efcb
Huy <huyltmce181964@fpt.edu.vn>
2024-12-10 16:01:21 +0700

    fix empty email

--- shoes-store-front/src/pages/Auth/ForgotPassword.jsx
+++ shoes-store-front/src/pages/Auth/ForgotPassword.jsx
@@ -12,6 +12,10 @@ const ForgotPassword = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
+		if (!accountEmail) {
+			enqueueSnackbar('Please enter your email to reset its password', { variant: 'error' })
+			return
+		}
 
 		const requestData = {
 			URL: AuthRequest.FORGOT_PASSWORD,
@@ -58,7 +62,7 @@ const ForgotPassword = () => {
 								onChange={(e) => setAccountEmail(e.target.value)}
 								className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 								placeholder='name@mail.com'
-								required=''
+								required
 							/>
 						</div>
 						<Button type='submit' size='lg' color='blue' fullWidth>
